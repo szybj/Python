92b48c39bf2b39f0b5deee89beee6944d1fe1ba6
songzhiyin <zhiyin.song@orange-king.com>
2019-05-17 18:36:34 +0800

    python 20190517

---
+++ hello.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+print 'Hello, World'
+print 45678 + 0x12fd2
+print 'Learn Python in imooc', 'asdasdasdadads'
+print 100 < 99
+print 0xff == 255
+print 'hello, python'
+print 'hello,', 'python'
+print '123'
+a = 'ABC'
+b = a
+a = 'DEF'
+print b  # 'ABC'
+c = '''Line 1
+Line 2
+Line 3'''
+f = r'''Python is created by "Guido".
+It is free and easy to learn.
+Let's start learn Python in imooc!'''
+print c
+print f
+
+print '''静夜思
+床前明月光，
+疑是地上霜。
+举头望明月，
+低头思故乡。'''
+print (1 + 2) * 3    # ==> 9
